eb06b4ebd0825a2b1647a61def5b1e8af38136f2
Craig M. Stimmel <cstimmel@uw.edu>
2017-05-22 13:57:19 -0700

    Hello world!

---
+++ ss_req.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+""" ss_req.py - super simple script to make a request to spotseeker_server.
+"""
+
+def main():
+    print "Hello world"
+
+if __name__ == '__main__':
+    main()
