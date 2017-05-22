be31b40c3a691c1d213b28cd67b668853e9fa313
Craig M. Stimmel <cstimmel@uw.edu>
2017-05-22 14:16:43 -0700

    Use requests_oauthlib to make a signed request.

--- ss_req.py
+++ ss_req.py
@@ -3,11 +3,15 @@
 """ ss_req.py - super simple script to make a request to spotseeker_server.
 """
 import settings
+import requests
+from requests_oauthlib import OAuth1
 
 def main():
-    print settings.URL
-    print settings.KEY
-    print settings.SECRET
+    url = settings.URL
+    auth = OAuth1(settings.KEY, settings.SECRET)
+
+    out = requests.get(url, auth=auth)
+    print out
 
 if __name__ == '__main__':
     main()
