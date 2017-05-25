4527dfcf981d3e973d385124b72bf5f1a2804e18
kevin <kevin@icecream.aca.uw.edu>
2017-05-25 10:45:50 -0700

    moving vulnerable information to secrets

--- sample.secrets.py
+++ sample.secrets.py
@@ -1,3 +1,9 @@
-URL = ''
-KEY = ''
-SECRET = ''
+# path to the server of the spotseeker data
+URL = "http://google.com"
+KEY = ""
+SECRET = ""
+
+
+ITEMS = {'Tech': '/api/v1/techitems', 
+        'Food': '/api/v1/fooditems', 
+        'Study': '/api/v1/studyitems'}
--- spot_mocker.py
+++ spot_mocker.py
@@ -10,19 +10,8 @@ import time
 import random
 from requests_oauthlib import OAuth1
 
-# path to the server of the spotseeker data
-server_url = "http://spotseeker-test-app1.cac.washington.edu"
-
 # path of where to store the mock data
 mock_path = "/home/kevin/Desktop/scout-vagrant/venv/src/scout/scout/resources/spotseeker/file"
-
-urls = ['/api/v1/spot?item%3Aextended_info%3Ai_is_active=true&limit=0&extended_info%3Acampus=seattle&extended_info%3Aapp_type=tech', 
-        '/api/v1/spot?limit=0&extended_info%3Acampus=seattle&extended_info%3Aapp_type=food', 
-        '/api/v1/spot?limit=0&extended_info%3Acampus=seattle&open_now=true']
-
-names = ["Kevin", "Mason", "Craig", "John", "Jim"]
-foods = ["Coffee", "Cafe", "Bistro", "Restaurant"]
-places = ["Library", "Hallway", "Hall", "Center"]
 
 def scrub(item):
 	return item
@@ -31,11 +20,11 @@ def main():
    	auth = OAuth1(secrets.KEY, secrets.SECRET)
    	print "How many Food/Study/Tech spots would you like?"
    	num = input();
-	for url in urls:
-		print "Generating spots for... " + url
+	for item_type, url in secrets.ITEMS.iteritems():
+		print "Generating spots for... " + item_type
 		mock_file_path = mock_path + url
 		mock_file = open(mock_file_path, 'w+')
-		resp = requests.get(server_url + url, auth=auth)
+		resp = requests.get(secrets.URL + url, auth=auth)
 		print len(resp.content)
 		data = json.loads(resp.content);
 		indexes = random.sample(range(0, len(data) - 1), min(num, len(data)))
@@ -50,4 +39,3 @@ def main():
 
 if __name__ == '__main__':
     out = main()
-    print out
