1dd9af21bbc7aca1bb53bd2d97215ca2a1b190ab
kevin <kevin@icecream.aca.uw.edu>
2017-05-25 11:56:51 -0700

    making some progress

--- spot_mocker.py
+++ spot_mocker.py
@@ -16,21 +16,46 @@ mock_path = "/home/kevin/Desktop/scout-vagrant/venv/src/scout/scout/resources/sp
 names = []
 foods = []
 places = []
+items = []
 
-def scrub(item):
+def get_name():
+	owner = ""
+	owner = random.choice(names)
+	return owner
+
+
+def get_item(space_type):
+	m = ""
+	if space_type == "Study":
+		m = random.choice(places)
+	elif space_type == "Tech":
+		m = random.choice(items)
+	else:
+		m = random.choice(foods)
+	return m
+
+def scrub(item, item_type):
 	#name, s_website_url, s_phone, s_support_email, owner, manager, images, location[building_name]
-	print item.get('name')
-	print item.get('s_website_url')
+	owner = get_name()
+	item_type = get_item(item_type)
+	item['name'] = owner + "\'s " + item_type 
+	print item['name']
 	return item
 
 def gather():
 	dir_path = os.path.dirname(os.path.realpath(__file__))
 	names_file = open(dir_path + "/names.txt", 'r')
-	names = names_file.read().split('\n')
+	global names
+	names = (filter(None, names + names_file.read().split('\n')))
 	foods_file = open(dir_path + "/foods.txt", 'r')
-	foods = foods_file.read().split('\n')
+	global foods
+	foods = (filter(None, foods + foods_file.read().split('\n')))
 	places_file = open(dir_path + "/places.txt", 'r')
-	places = places_file.read().split('\n')
+	global places
+	places = (filter(None, places + places_file.read().split('\n')))
+	items_file = open(dir_path + "/items.txt", 'r')
+	global items
+	items = (filter(None, items + items_file.read().split('\n')))
 
 def main():
 	gather()
@@ -42,14 +67,13 @@ def main():
 		mock_file_path = mock_path + url
 		mock_file = open(mock_file_path, 'w+')
 		resp = requests.get(secrets.URL + url, auth=auth)
-		print len(resp.content)
 		data = json.loads(resp.content);
 		indexes = random.sample(range(0, len(data) - 1), min(num, len(data)))
 		items = []
 		for index in indexes:
 			# study/food/tech space
 			item = data[index]
-			clean_space = scrub(item);
+			clean_space = scrub(item, item_type);
 			# print clean_space
 			print ""
 			print ""
